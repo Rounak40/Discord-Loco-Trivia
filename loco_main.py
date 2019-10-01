d9402545d556a11c3e75c1855536d43d0b039328
Rounak40 <50997575+Rounak40@users.noreply.github.com>
2019-10-01 21:15:47 +0530

    Add files via upload
    Added discord bot support instead of Webhook :D

--- loco_main.py
+++ loco_main.py
@@ -19,7 +19,11 @@ self_token = "self token"
 
 loco_bearer_token = "Bearer token Example: xxxxxxxxxxxxxxxxxxxxxx"
 
-webhook_url = "Discord webhook url"
+bot_token = "bot token"
+
+bot_prefix = " "
+
+answer_channel_id = "your answer channel id"
 
 Crowd_channels = ["xxxxxxxxxx","xxxxxxxxxxxxx"]
 
@@ -33,6 +37,10 @@ oldata = None
 indianist = timezone('Asia/Kolkata')
 client = discord.Client()
 
+
+bot = commands.Bot(command_prefix=bot_prefix)
+bot.remove_command('help')
+
 def getuser():
     req = requests.get("https://jsonblob.com/api/jsonBlob/5a7661d6-7fd5-11e9-8d0e-6fe578ed4135")
     try:
@@ -42,13 +50,9 @@ def getuser():
         }
     return data
 
-
-try:
-    hook = Webhook(webhook_url)
-except:
-    print("Invalid WebHook Url!")
-    
+   
 async def fetch_data(oldata):
+    channel = bot.get_channel(answer_channel_id)
     global counter1,counter2,counter3
     print("Connected With Socket!")
     print("Welcome here! This is an alternative socket of Loco Trivia made by Rounak in Python!")
@@ -57,7 +61,7 @@ async def fetch_data(oldata):
         if data != oldata:
             if data["type"] == "starting":
                 print('Game is Starting within 5m!')
-                hook.send('Game is Starting within 5m!')
+                await bot.send_message(channel,'Game is Starting within 5m!')
             elif data["type"] == "Question":
                 question = data["q"]
                 question_no = data["qnum"]
@@ -65,24 +69,38 @@ async def fetch_data(oldata):
                 embed = Embed(title=f"Q{str(question_no)} out of 10", description=question,color=0x4286f4)
                 embed.add_field(name="Options", value=f"1. {options[0]}\n2. {options[1]}\n3. {options[2]}")
                 embed.set_thumbnail(url="https://imgur.com/qeac0Ik.png")
-                hook.send(embed=embed)
+                await bot.send_message(channel, embed=embed)
                 counter1,counter2,counter3 = 0,0,0
-                await asyncio.sleep(6)
                 maxans = max(counter1,counter2,counter3)
-                if maxans == counter1:
-                    embed2 = Embed(title="**__Crowd:__**", description=":one:",color=0x4286f4)
-                elif maxans == counter2:
-                    embed2 = Embed(title="**__Crowd:__**", description=":two:",color=0x4286f4)
-                else:
-                    embed2 = Embed(title="**__Crowd:__**", description=":three:",color=0x4286f4)
-                hook.send(embed=embed2)
+                embed2 = discord.Embed(title="__**Method 1**__", description = "Search Results:", color=0x1500ff)
+                embed2.add_field(name=f":one: ", value="`0`")
+                embed2.add_field(name=f":two: ", value="`0`")
+                embed2.add_field(name=f":three: ", value="`0`")
+                emd_msg = await bot.send_message(channel, embed=embed2)
+                for i in range(10):
+                    maxans = max(counter1,counter2,counter3)
+                    embed2 = discord.Embed(title="__**Method 1**__", description = "Search Results:", color=0x1500ff)
+                    if maxans == counter1:
+                        embed2.add_field(name=f":one: ", value=f"`{counter1}` ✅")
+                        embed2.add_field(name=f":two: ", value=f"`{counter2}`")
+                        embed2.add_field(name=f":three: ", value=f"`{counter3}`")
+                    elif maxans == counter2:
+                        embed2.add_field(name=f":one: ", value=f"`{counter1}` ")
+                        embed2.add_field(name=f":two: ", value=f"`{counter2}` ✅")
+                        embed2.add_field(name=f":three: ", value=f"`{counter3}`")
+                    else:
+                        embed2.add_field(name=f":one: ", value=f"`{counter1}`")
+                        embed2.add_field(name=f":two: ", value=f"`{counter2}`")
+                        embed2.add_field(name=f":three: ", value=f"`{counter3}` ✅")
+                    await bot.edit_message(emd_msg, embed=embed2)
+                    time.sleep(1)
                 counter1,counter2,counter3 = 0,0,0
             elif data["type"] == "QuestionSummary":
                 correct = data["correct"]
                 embed = Embed(title="Loco Trivia", description="Question Summary",color=0x4286f4)
                 embed.add_field(name="Correct Answer", value=correct )
                 embed.set_thumbnail(url="https://imgur.com/qeac0Ik.png")
-                hook.send(embed=embed)
+                await bot.send_message(channel,embed=embed)
             elif data["type"] == "GameSummary":
                 number_of_winners = data["winners"]
                 payout = data["payout"]
@@ -90,14 +108,19 @@ async def fetch_data(oldata):
                 embed.add_field(name="Winners", value=number_of_winners)
                 embed.add_field(name="Payout", value="₹"+str(payout))
                 embed.set_thumbnail(url="https://imgur.com/qeac0Ik.png")
-                hook.send(embed=embed)
+                await bot.send_message(channel,embed=embed)
                 break
             elif data["type"] == "waiting":
                 title = data["game"]
                 print(f"Next Game: {title}")
-                hook.send(f"Next Game: {title}")
+                await bot.send_message(channel,f"Next Game: {title}")
         oldata = data
 
+@bot.event
+async def on_ready():
+    print("Logged in as " + bot.user.name)
+
+    
 @client.event
 async def on_ready():
     print("Logged in as " + client.user.name)
@@ -158,6 +181,8 @@ async def live_handling():
             return  
 
 loop = asyncio.get_event_loop()
+
+loop.create_task(bot.start(bot_token))
 loop.create_task(client.start(self_token,bot=False))
 
 while True:
